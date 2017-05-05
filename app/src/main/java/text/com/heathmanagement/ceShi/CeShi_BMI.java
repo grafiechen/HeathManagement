57bb4b64930cf0b47b08b59111531d2669fdbbbe
jinpeng96 <835753853@qq.com>
2017-05-05 20:35:20 +0800

    11111

--- app/src/main/java/text/com/heathmanagement/ceShi/CeShi_BMI.java
+++ app/src/main/java/text/com/heathmanagement/ceShi/CeShi_BMI.java
@@ -1,8 +1,18 @@
 package text.com.heathmanagement.ceShi;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import text.com.heathmanagement.R;
+
 /**
  * Created by 陈锦鹏 on 2017/4/11.
  */
 
-public class CeShi_BMI {
+public class CeShi_BMI extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ceshi_bmi);
+    }
 }
