3c893da43f67377c27ff9fe2da66c5ca96f44d59
jinpeng96 <835753853@qq.com>
2017-05-05 23:06:53 +0800

    2017-05-05 23:05:53 完善

---
+++ app/src/main/java/text/com/heathmanagement/dao/hmApplication.java
@@ -0,0 +1,10 @@
+package text.com.heathmanagement.dao;
+
+import org.litepal.LitePalApplication;
+
+/**
+ * Created by 陈锦鹏 on 2017/5/5.
+ */
+
+public class hmApplication extends LitePalApplication {
+}
