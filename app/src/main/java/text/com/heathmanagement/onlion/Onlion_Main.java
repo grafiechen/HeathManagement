12eab5fef7b572cb11f9a0ec8a4196d622787df2
jinpeng96 <835753853@qq.com>
2017-05-05 20:18:28 +0800

    完善

--- app/src/main/java/text/com/heathmanagement/onlion/Onlion_Main.java
+++ app/src/main/java/text/com/heathmanagement/onlion/Onlion_Main.java
@@ -1,17 +1,8 @@
 package text.com.heathmanagement.onlion;
 
-import android.app.Activity;
-import android.os.Bundle;
-
-import text.com.heathmanagement.R;
-
 /**
  * Created by 陈锦鹏 on 2017/4/11.
  */
 
-public class Onlion_Main extends Activity{
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class Onlion_Main {
 }
