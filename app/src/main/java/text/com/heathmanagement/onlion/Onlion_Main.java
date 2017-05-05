e3df8feb895a691144a9f0e4b161686fde5175ec
jinpeng96 <835753853@qq.com>
2017-05-05 19:19:42 +0800

    Rejected commit: Default

--- app/src/main/java/text/com/heathmanagement/onlion/Onlion_Main.java
+++ app/src/main/java/text/com/heathmanagement/onlion/Onlion_Main.java
@@ -1,8 +1,17 @@
 package text.com.heathmanagement.onlion;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import text.com.heathmanagement.R;
+
 /**
  * Created by 陈锦鹏 on 2017/4/11.
  */
 
-public class Onlion_Main {
+public class Onlion_Main extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
